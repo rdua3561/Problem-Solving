034399dc6873244ea046a9e7424fb39cc9f2e43b
Rajat Dua <rdua3561@gmail.com>
2022-02-16 22:59:34 +0530

    Time: 25 ms (52.86%), Space: 19.7 MB (52.26%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+//         int q=nums.size();
+//         int maxi=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]>maxi)
+//                 maxi=nums[i];
+//         }
+//         vector<int> c(maxi);
+//           for(int i=0;i<nums.size();i++)
+//         {
+//               c[i]=0;
+//           }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             c[nums[i]]+=1;
+//         }
+//         int max=c[0];
+//          for(int i=1;i<nums.size();i++)
+//         {
+//              if(c[i]>max)
+//                  max=c[i];
+//          }
+        
+//         return max;
+        
+        unordered_map<int,int> counter;
+        
+        for(auto i: nums)
+        {
+            if(++counter[i]> nums.size()/2)
+                return i;
+        }
+        
+        return 0;
+        
+    }
+};
