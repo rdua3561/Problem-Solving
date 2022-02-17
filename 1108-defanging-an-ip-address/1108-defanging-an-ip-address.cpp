7cbd9b547d1192270860d3b1824874eb548b87c8
Rajat Dua <rdua3561@gmail.com>
2022-02-17 23:51:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.58%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string res;
+        for(auto c: address) {
+            if(c=='.') {
+                res+="[.]";
+            }
+            
+            else 
+                res+=c;
+        }
+        return res;
+    }
+        
+    
+};
