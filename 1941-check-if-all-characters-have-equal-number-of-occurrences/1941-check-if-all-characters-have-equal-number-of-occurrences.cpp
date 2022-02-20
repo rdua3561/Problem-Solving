cdfd040ba80fa4e687a26ef60fdbd5eae425ed4f
Rajat Dua <rdua3561@gmail.com>
2022-02-20 20:17:33 +0530

    Time: 4 ms (74.70%), Space: 6.7 MB (61.48%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        
+        vector<int> cnt(26,0);
+		
+        for(char c : s) 
+            cnt[c-'a']++;
+		
+        int num=cnt[s[0]-'a'];
+        for(int i: cnt){
+            if(i!=num and i!=0) return false;
+        }
+        return true;
+    
+    }
+};
