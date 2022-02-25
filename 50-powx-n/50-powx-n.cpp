4e21180230e6d9130c62817e90d9b6819116c8ed
Rajat Dua <rdua3561@gmail.com>
2022-02-25 09:43:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.26%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -8,10 +8,10 @@ public:
     }
         
       if(n%2==0) 
-      return myPow(x*x, n/2) ;
+        return myPow(x*x, n/2) ;
         
         else
-        return x*myPow(x*x, n/2);
+            return x*myPow(x*x, n/2);
         
     }
 };
