b9568940e055f2f5899711d6581a1ab9d7a844ca
Rajat Dua <rdua3561@gmail.com>
2022-02-27 15:07:28 +0530

    Time: 6 ms (19.37%), Space: 6.1 MB (78.89%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        int c[256]={0};
+    //METHOD 1
+//     int numJewelsInStones(string jewels, string stones) {
+//         int c[256]={0};
         
-        for(int i=0;i < stones.length();i++)
-        {
-            c[stones[i]-'0']++;
+//         for(int i=0;i < stones.length();i++)
+//         {
+//             c[stones[i]-'0']++;
             
-        }
-        int counter=0;
+//         }
+//         int counter=0;
         
-         for(int i=0;i < jewels.length();i++)
-        {
-        for(int j=0;j<stones.length();j++)
-        {
-            if(jewels[i]==stones[j] &&  c[stones[j]-'0']>0)
-            {
-                counter++;
-                c[stones[j]-'0']--;
+//          for(int i=0;i < jewels.length();i++)
+//         {
+//         for(int j=0;j<stones.length();j++)
+//         {
+//             if(jewels[i]==stones[j] &&  c[stones[j]-'0']>0)
+//             {
+//                 counter++;
+//                 c[stones[j]-'0']--;
                 
-            }
-        }
-         }
-        return counter;
+//             }
+//         }
+//         }
+//         return counter;
+//     }
+    
+    //METHOD 2 Short and Crisp
+    int numJewelsInStones(string jewels, string stones) {
+    int counter = 0;
+    for (int i = 0; i < jewels.size(); i++){
+        counter += count(stones.begin(), stones.end(), jewels[i]);
     }
+    return counter;
+}
 };
