ce50092ea73de88618feffb8bb1b99ad6fc4a9dc
Rajat Dua <rdua3561@gmail.com>
2022-03-03 23:59:50 +0530

    Time: 3 ms (66.63%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        if( nums.size()<3)
-            return 0;
+int numberOfArithmeticSlices(vector<int>& nums) {
+    if( nums.size()<3)
+        return 0;
+    
+    int c=0;
+    int total=0;
+    for(int i=2;i<nums.size();i++){         //Starting when array Size >= 3
         
-        int c=0;
-        int total=0;
-        for(int i=2;i<nums.size();i++){
+        if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])      //Compairing the  difference like you do in AP
+        {
+            c++;   //that particular no. of subarray is stored in 'c' that was checked in if condition
+            total=total+c;  //total number of subarray stored
+        }
+        
+        else
+        {
+            c=0;
             
-            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
-            {
-                c++;
-                total=total+c;
-            }
-            
-            else
-            {
-                c=0;
-                
-            }
+        }
 
-            }
-        return total;
-    }
+        }
+    return total;
+}
 };
