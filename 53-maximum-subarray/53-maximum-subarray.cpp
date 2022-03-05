baf24dbab2d2888e10c62b212856a41c024c9a72
Rajat Dua <rdua3561@gmail.com>
2022-03-05 20:31:16 +0530

    Time: 108 ms (84.85%), Space: 67.6 MB (99.64%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //Kadanes Algorithm
+        int n = nums.size();
+        int max=INT_MIN;
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            
+            if(sum>max)
+                max=sum;
+            
+            if(sum<0) 
+                sum=0;
+        }
+        return  max;
+    }
+        
+    
+};
