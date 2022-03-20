fc58dd1754e3fb7f55435aa9cb30adbaa3e3fcdb
Rajat Dua <rdua3561@gmail.com>
2022-03-20 23:43:47 +0530

    Time: 11 ms (19.48%), Space: 11.5 MB (56.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -8,19 +8,20 @@ public:
         for(int i=0;i<word1.size();i++)
         {
             s1+=word1[i];
-         
+   
         }
-        
-         for(int i=0;i<word2.size();i++)
+        for(int i=0;i<word2.size();i++)
         {
-       
+           
             s2+=word2[i];
         }
-
-           
+        
+        
+            if(s1!=s2)
+                return 0;
         
      
-                return s1==s2;
+                return 1;
         
     }
 };
