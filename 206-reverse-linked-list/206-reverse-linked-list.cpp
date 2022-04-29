e5d58499bfc84179d4791ef50b72210caec47e70
Rajat Dua <rdua3561@gmail.com>
2022-04-29 14:02:46 +0530

    Time: 8 ms (60.80%), Space: 8.2 MB (97.46%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,35 +11,48 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //Method 1
+//         int i=0;
+//         int c=0;
+//         ListNode* p=head;
+//          while(p!=NULL)
+//         {
+//            c++;
+//             p=p->next;
+//         }
         
-        int i=0;
-        int c=0;
-        ListNode* p=head;
-         while(p!=NULL)
+//         int a[c+1];
+//         p=head;
+//         while(p!=NULL)
+//         {
+//             a[i++]=p->val;
+//             p=p->next;
+//         }
+        
+//         i--;
+//         p=head;
+        
+//          while(p!=NULL)
+//         {
+//             p->val=a[i];
+//              i--;
+//             p=p->next;
+//         }
+        
+//         return head;
+        //Method 2
+        
+        
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* temp=NULL;
+        while(curr!=NULL)
         {
-           c++;
-            p=p->next;
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
-        
-        int a[c+1];
-        p=head;
-        while(p!=NULL)
-        {
-            a[i++]=p->val;
-            p=p->next;
-        }
-        
-        i--;
-        p=head;
-        
-         while(p!=NULL)
-        {
-            p->val=a[i];
-             i--;
-            p=p->next;
-        }
-        
-        return head;
-        
+        return prev;
     }
 };
