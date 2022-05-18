2d5dba20a5d1c8047f514339c5fc0be7319bb872
Rajat Dua <rdua3561@gmail.com>
2022-05-19 01:30:42 +0530

    Time: 161 ms (20.29%), Space: 54.8 MB (11.02%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+         set<int> s;
+		
+        s.insert(nums.begin(),nums.end()); 
+		
+        if (nums.size()!=s.size())
+		
+            return true;
+        
+        else
+            return false;
+        
+    }
+};
