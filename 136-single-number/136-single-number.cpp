4d12f9fa95a2af928acf33beda8cd1ab1dc0182a
Rajat Dua <rdua3561@gmail.com>
2022-05-19 10:55:56 +0530

    Time: 38 ms (26.02%), Space: 20 MB (19.60%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,8 +1,8 @@
 
 class Solution {
 public:
-//     int singleNumber(vector<int>& nums) {
-         
+    // int singleNumber(vector<int>& nums) {
+        // METHOD 1 (Using XOR)
 //         int ans=0;
         
 //         for(int i=0;i<nums.size();i++)
@@ -10,18 +10,36 @@ public:
 //             ans=ans^nums[i];
 //         }
         
-//         return ans;
-        
+//         return ans;    
         
 //     }
     
-    int singleNumber(vector<int>& nums) { 
-       sort(nums.begin() ,nums.end());
+    //Method 2
+//     int singleNumber(vector<int>& nums) { 
+//        sort(nums.begin() ,nums.end());
         
-        for(int i=1; i<nums.size(); i+=2){ //i+=2 as we want to check every other element
-            if(nums[i]!=nums[i-1])
-                return nums[i-1];
+//         for(int i=1; i<nums.size(); i+=2){ //i+=2 as we want to check every other element
+//             if(nums[i]!=nums[i-1])
+//                 return nums[i-1];
+//         }
+//         return nums[nums.size()-1]; //if we didnot find answer in the loop, so return the last element.
+//     }
+int singleNumber(vector<int>& nums) { 
+      //Method 3:Hashing(through unordered map)
+    unordered_map<int, int> mp;
+        for(auto i: nums)
+        {
+            mp[i]++;
         }
-        return nums[nums.size()-1]; //if we didnot find answer in the loop, so return the last element.
-    }
+    
+        for(auto i: mp)
+        {
+            if(i.second==1)
+                return i.first;
+        }
+    
+    return -1;
+        
+
+   }
 };
