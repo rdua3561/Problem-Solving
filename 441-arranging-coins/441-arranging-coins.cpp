a1b8146cfbe97b0c380ac2b7c77cd5732551908b
Rajat Dua <rdua3561@gmail.com>
2022-05-26 22:10:00 +0530

    Time: 3 ms (78.31%), Space: 5.9 MB (72.02%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
+   // Method 1:Maths
     int arrangeCoins( int n) {
+        //Approach: (k*(k+1))/2 <= n
         
       return sqrt(2*(double)n+0.25)-0.5;
 
     }
+    
+    //Method 2: Binary Search
+//      int arrangeCoins( int n) {
+     
+//          int h=n,l=0;
+         
+//          while(l<=h)
+//          {
+//              int mid=l+(h-l)/2;
+             
+//              if(mid*(mid+1)/2<=n)
+//                  l=mid+1;
+                 
+            
+//                  else
+//                      h=mid-1;
+//          }
+         
+//          return l-1;
+
+//     }
 };
