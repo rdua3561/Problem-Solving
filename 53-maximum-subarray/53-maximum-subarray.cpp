3d837283cf5439f6b063e58a333a3612fccd31a7
Rajat Dua <rdua3561@gmail.com>
2022-05-28 21:17:18 +0530

    Time: 166 ms (30.81%), Space: 67.6 MB (99.50%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
+        //Method 1: Brute Force(O^3)  TLE
+//          int maxSubArray(vector<int>& nums) {
+
+//           int n = nums.size();
+//          int largest_sum=INT_MIN;
+//          for(int i=0;i<n;i++)
+//          {
+//              for(int j=i;j<n;j++)
+//              {
+//                  int sum=0;
+//                for(int k=i;k<=j;k++)
+//                {
+//                    sum=sum+nums[k];
+                   
+//                }
+//                  largest_sum=max(sum,largest_sum);
+//              }
+//          }
+//          return largest_sum;
+     
+//     }
+    
     int maxSubArray(vector<int>& nums) {
-        //Kadanes Algorithm
+        //Method 1: Kadanes Algorithm
         int n = nums.size();
         int ultra_max=INT_MIN;
         int sum=0;
@@ -12,12 +34,11 @@ public:
             ultra_max=max(sum,ultra_max);
             if(sum<0)
                 sum=0;
-            
-            
       
       }
         return ultra_max;
     }
         
+
     
 };
