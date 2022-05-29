33a19a2efddf8d3264bd9df785e2340ff3633331
Rajat Dua <rdua3561@gmail.com>
2022-05-29 14:46:13 +0530

    Time: 147 ms (47.77%), Space: 67.7 MB (55.05%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-        //Method 1: Brute Force(O^3)  TLE
+//         //Method 1: Brute Force(O^3)  TLE
 //          int maxSubArray(vector<int>& nums) {
 
 //           int n = nums.size();
@@ -21,11 +21,35 @@ public:
 //          return largest_sum;
      
 //     }
+//     //Method 2: Prefix Sum O(N^2)  TLE for Large Number of Inputs
+//      int maxSubArray(vector<int>& nums) {
+     
+//          //Creating Prefix sum array
+//          int n=nums.size();
+//          int prefix_sum[n+1];
+//          prefix_sum[0]=nums[0];
+//          for(int i=1;i<n;i++)
+//          {
+//              prefix_sum[i]=prefix_sum[i-1]+nums[i];
+//          }
+//          //creating of Prefix sum array just done above
+         
+//          int largest_sum=INT_MIN;
+//          for(int i=0;i<n;i++)
+//          {
+//              for(int j=i;j<n;j++)
+//              {
+                
+//                  int subarray_sum= i>0 ?(prefix_sum[j]-prefix_sum[i-1]):prefix_sum[j] ; // all subarray sum is generated
+//                 largest_sum=max(subarray_sum, largest_sum);     // max of subarray sum goes in to our largest_sum
+//              }
+//          }
+//          return largest_sum;
+//     }
     
-    
-    //Method 1: Kadanes Algorithm
+//     //Method 3: Kadanes Algorithm
 //     int maxSubArray(vector<int>& nums) {
-//         
+        
 //         int n = nums.size();
 //         int ultra_max=INT_MIN;
 //         int sum=0;
@@ -44,14 +68,14 @@ public:
 //Method 3: Kadanes Algorithm Modified Version
         int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int ultra_max=nums[0];
+        int max_ending=nums[0];
         int res=nums[0];
         
         for(int i=1;i<n;i++){
             
-            
-            ultra_max=max(ultra_max+nums[i],nums[i]);
-            res=max(ultra_max,res);
+            //LOGIC= max_ending(i)=max( max_ending(i-1)+nums[i] , nums[i])
+            max_ending=max(max_ending+nums[i],nums[i]);
+            res=max(max_ending,res);
             
       }
         return res;
