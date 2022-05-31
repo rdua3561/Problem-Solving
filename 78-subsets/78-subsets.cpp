3b9b8c3ce52cbccb87c91a6a0ffa48fa08dafaac
Rajat Dua <rdua3561@gmail.com>
2022-05-31 20:54:36 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (53.38%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,19 +2,22 @@ class Solution {
     
     public:
 vector<vector<int>> v;
-   void help(vector<int>& nums, int i, vector<int>& st)
+void help(vector<int>& nums,int i,vector<int>& st)
+{
+    if(i==nums.size())
     {
-        
-        if(i==nums.size())
-        {
-            v.push_back(st);
-            return;
-        }
-        st.push_back(nums[i]);
-       help(nums,i+1,st);
-       st.pop_back();
-       help(nums,i+1,st);
+        v.push_back(st);
+        return;
     }
+    
+    //pick
+    st.push_back(nums[i]);
+    help(nums,i+1,st);
+    st.pop_back();
+    
+    //not pick
+    help(nums,i+1,st);
+}
 
     vector<vector<int>> subsets(vector<int>& nums) {
         
