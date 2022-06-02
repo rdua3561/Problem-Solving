55d60c5ff81b98c2212b5a44c1cf81af1c7e6c9c
Rajat Dua <rdua3561@gmail.com>
2022-06-03 00:33:43 +0530

    Added solution - LeetHub

--- Value equal to index value - GFG/value-equal-to-index-value.cpp
+++ Value equal to index value - GFG/value-equal-to-index-value.cpp
@@ -14,7 +14,7 @@ vector<int> valueEqualToIndex(int arr[], int n) {
      vector<int>v;
     for(int i=0; i<n;i++){
         if(i+1==arr[i]){
-            v.push_back(arr[i]);
+            v.push_back(i+1);
         }
     }
     return v;
