90f6cd30b8098467a68c56c378b311d5879550b6
Rajat Dua <rdua3561@gmail.com>
2022-06-04 01:02:03 +0530

    Time: 138 ms (9.44%), Space: 9.7 MB (46.73%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                for(int j=n-1;j>=i+1;j--)
+                    arr[j]=arr[j-1];
+                
+             i=i+1;
+            }
+           
+        }
+        
+    }
+};
+
