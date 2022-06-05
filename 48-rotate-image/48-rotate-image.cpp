d216d88c5379b9b67ecc1524097314bd7c5ae0c3
Rajat Dua <rdua3561@gmail.com>
2022-06-05 15:09:03 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.00%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -21,13 +21,14 @@ public:
 
       
 //     }
+        
+        //MIRROR IMAGE CODE
     for(i=0;i<n;i++)
      {
         for( j=0;j<n/2;j++)
         {
             swap(matrix[i][j],matrix[i][n-j-1]);
         }
-     
-    }
+     }
     }
 };
