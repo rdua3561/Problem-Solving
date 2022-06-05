f9440725c56dd5ecf20dc0aaa0990d2afc61b61b
Rajat Dua <rdua3561@gmail.com>
2022-06-05 15:07:42 +0530

    Time: 5 ms (26.75%), Space: 6.9 MB (92.51%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -14,11 +14,20 @@ public:
      }
    
     //REVERSE EACH ROW OF A MATRIX
-    for(i=0;i<n;i++)
-    {
-        reverse(matrix[i].begin(),matrix[i].end());
-    }
+//     for(i=0;i<n;i++)
+//     {
+//         reverse(matrix[i].begin(),matrix[i].end());
+//     }
 
       
+//     }
+    for(i=0;i<n;i++)
+     {
+        for( j=0;j<n/2;j++)
+        {
+            swap(matrix[i][j],matrix[i][n-j-1]);
+        }
+     
+    }
     }
 };
