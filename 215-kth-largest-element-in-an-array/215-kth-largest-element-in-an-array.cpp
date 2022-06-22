b565dde6f1922f70a71281e23da4e1d1f41abf20
Rajat Dua <rdua3561@gmail.com>
2022-06-22 22:35:26 +0530

    Time: 17 ms (29.72%), Space: 10.2 MB (21.99%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -6,19 +6,28 @@ public:
         // return nums[k-1];
         
         // METHOD 2: Heaps
-        priority_queue<int> pq;
+//         priority_queue<int> pq;
         
-        for(auto i: nums)
-        {
-            pq.push(i);
-        }
+//         for(auto i: nums)
+//         {
+//             pq.push(i);
+//         }
         
-            while(k!=1)
-            {
-                pq.pop();
-                k--;
-            }
-        return pq.top();
+//             while(k!=1)
+//             {
+//                 pq.pop();
+//                 k--;
+//             }
+//         return pq.top();
+        
+        //Method 3: Priority Queue Improvement
+    priority_queue<int,vector<int>,greater<int>> pq;
+        
+	for(int i=0;i<nums.size();++i){
+		pq.push(nums[i]);
+		if(pq.size()>k) pq.pop();
+	}
+	return pq.top();   
         
     }
 };
