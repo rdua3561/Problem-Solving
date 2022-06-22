0a69e39990c21b00a39e9e1355fc001f725ccc7f
Rajat Dua <rdua3561@gmail.com>
2022-06-23 04:46:30 +0530

    Time: 35 ms (61.08%), Space: 18.9 MB (31.32%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> v;
+    
+    void help(int i,int n,int k)
+    {
+        if(k==0)
+        {
+            ans.push_back(v);
+            return;
+        }
+        
+        if(i>n)
+        {
+            
+            return;
+        }
+       
+       
+        //Pick
+            v.push_back(i);
+            help(i+1,n,k-1);
+            v.pop_back();
+        
+        //not pick
+        help(i+1,n,k);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        
+        help(1,n,k);
+        return ans;
+    }
+    
+};
