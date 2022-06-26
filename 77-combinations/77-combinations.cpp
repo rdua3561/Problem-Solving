264e389b54c5bb09abc65bdee85b68ce0d9b409a
Rajat Dua <rdua3561@gmail.com>
2022-06-26 07:38:48 +0530

    Time: 29 ms (77.59%), Space: 10 MB (52.66%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> v;
-    
-    void help(int i,int n,int k)
+    //METHOD 1: Using for Loop
+vector<vector<int>> ans;
+void help(int i,int n,int k,vector<int> &v)
     {
         if(k==0)
         {
@@ -11,24 +10,47 @@ public:
             return;
         }
         
-        if(i>n)
+       
+        for(int j=i;j<=n;j++)
         {
-            return;
-        }
-       
-       
-        //Pick
-            v.push_back(i);
-            help(i+1,n,k-1);
+            v.push_back(j);
+            help(j+1,n,k-1,v);
             v.pop_back();
-        
-        //not pick
-        help(i+1,n,k);
+    
+        }
+    
+      return;
     }
+
     vector<vector<int>> combine(int n, int k) {
-        
-        help(1,n,k);
+        int i=1;
+        vector<int> v;
+        help(i,n,k,v);
         return ans;
     }
     
 };
+
+//Method 2: without using for loop with 2 base conditions
+// vector<vector<int>> ans;
+// vector<int> v;
+//     void help(int i,int n,int k)
+//     {
+//         if(k==0)
+//         {
+//             ans.push_back(v);
+//             return;
+//         }
+//         if(i>n)
+//             return;
+       
+       
+       
+//         //Pick
+//             v.push_back(i);
+//             help(i+1,n,k-1);
+//             v.pop_back();
+        
+//         //not pick
+//         help(i+1,n,k);
+//     }
