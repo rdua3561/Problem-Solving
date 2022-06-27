839f751fddeccdc00e67304a479ce3b8a70c8b27
Rajat Dua <rdua3561@gmail.com>
2022-06-27 22:13:53 +0530

    Time: 3 ms (43.37%), Space: 6 MB (60.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
+    //Method 1: used dp as a vector
     
-    int help(int i,int j,int m,int n,vector<vector<int>>& dp)
+//     int help(int i,int j,int m,int n,vector<vector<int>>& dp)
+//     {
+//         if(i>=m ||j>=n)  return 0;
+//         if(i==m-1 and j==n-1)  return 1;
+        
+//         if(dp[i][j]!=-1)
+//         {
+//             return dp[i][j];
+//         }
+        
+        
+//         return dp[i][j]=help(i+1,j,m,n,dp) + help(i,j+1,m,n,dp);
+        
+    
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return help(0,0,m,n,dp);
+//     }
+    //Method 3: used dp in global and used memset to make -1 all dp
+    
+    int dp[102][102];
+      int help(int i,int j,int m,int n)
     {
         if(i>=m ||j>=n)  return 0;
         if(i==m-1 and j==n-1)  return 1;
@@ -12,12 +35,12 @@ public:
         }
         
         
-        return dp[i][j]=help(i+1,j,m,n,dp) + help(i,j+1,m,n,dp);
+        return dp[i][j]=help(i+1,j,m,n) + help(i,j+1,m,n);
         
     
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return help(0,0,m,n,dp);
+        memset(dp,-1,sizeof(dp));
+        return help(0,0,m,n);
     }
 };
