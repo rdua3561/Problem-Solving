a90b11fca0f2d8ae97b50bf6145b9ec00ee73f91
Rajat Dua <rdua3561@gmail.com>
2022-06-29 00:41:35 +0530

    Time: 4 ms (88.62%), Space: 13.8 MB (27.86%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -5,7 +5,7 @@ public:
     {
         if(n==0) return cost[0];
         if(n==1) return cost[1];
-        
+        if(n<0)  return 0;
         if(dp[n]!=-1) return dp[n];
         
          dp[n]=cost[n]+min(help(n-1,cost,dp),help(n-2,cost,dp));
