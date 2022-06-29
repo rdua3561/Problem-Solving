e7baeb8f8566ebd4e436e293819fa9f238866e29
Rajat Dua <rdua3561@gmail.com>
2022-06-30 05:28:17 +0530

    Time: 3 ms (57.54%), Space: 7.9 MB (41.29%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
     
-   int solve(vector<int>&nums,int i,int n,vector<int>&dp)
-    {
-        if(i>=n-1)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
+//     int help(vector<int> &nums,int i)
+//     {
+       
+//         if(i>=nums.size())
+//         {
+//             return 0;
+//         }
+//      \
+//         int include=nums[i]+help(nums,i+2);
+//         int exclude=help(nums,i+1);
         
-        return dp[i]=max(nums[i]+solve(nums,i+2,n,dp),solve(nums,i+1,n,dp));
+//         return max(include,exclude);
+        
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         if(nums.size()==1)
+//         {
+//             return nums[0];
+//         }
+        
+//         //memset(dp,-1,sizeof(dp));
+//         vector<int> first_exclude(nums.begin()+1,nums.end());
+//         vector<int> last_exclude(nums.begin(),nums.end()-1);
+        
+//         return max(help(first_exclude,0),help(last_exclude,0));
+    // }
+    
+     int houseRobber(vector<int>& nums) {
+        // dynamic programming - decide each problem by its sub-problems:
+        int dp[nums.size()+1];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i=2; i<nums.size(); i++)
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+        return dp[nums.size()-1];
     }
-     int solve2(vector<int>&nums,int i,int n,vector<int>&dp)
-    {
-        if(i>=n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-          return dp[i]=max(nums[i]+solve2(nums,i+2,n,dp),solve2(nums,i+1,n,dp));
-     }
+    
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp1(n,-1);
-        vector<int>dp2(n,-1);
-       if(n==1)return nums[0];
+        // edge cases:
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
         
-        int ans1=solve(nums,0,n,dp1);
-        int ans2=solve2(nums,1,n,dp2);
-        return max(ans1,ans2);
+        // either use first house and can't use last or last and not first:
+        vector<int> v1(nums.begin(), nums.end()-1);
+        vector<int> v2(nums.begin()+1, nums.end());
+        return max(houseRobber(v1), houseRobber(v2));
     }
 };
+
