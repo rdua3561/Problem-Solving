26858dc37cdbc67cca41132dae5a5d2958061a7d
Rajat Dua <rdua3561@gmail.com>
2022-07-04 03:36:58 +0530

    Time: 12 ms (15.76%), Space: 12.6 MB (31.29%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,29 +12,57 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+       queue<TreeNode*> q;
         vector<vector<int>> ans;
-        queue<TreeNode*> q;
-        if( root ==NULL) return ans;
-        
+        if(root==NULL) return ans;
         q.push(root);
         while(!q.empty())
         {
-             vector<int> v;
-            int n=q.size();
-            for(int i=0;i<n;i++)
+            int sz=q.size();
+            vector<int> v;
+            for(int i=1;i<=sz;i++)
             {
-            auto x=q.front();
-            q.pop();
-            v.push_back(x->val);
+               auto temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
                 
-                if(x->left!=NULL) q.push(x->left);
-                if(x->right!=NULL) q.push(x->right);
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+                 
                 
             }
             ans.push_back(v);
             
         }
-        return ans;
         
+        return ans;
     }
 };
+
+
+
+
+
+//  vector<vector<int>> ans;
+//         queue<TreeNode*> q;
+//         if( root ==NULL) return ans;
+        
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//              vector<int> v;
+//             int n=q.size();
+//             for(int i=0;i<n;i++)
+//             {
+//             auto x=q.front();
+//             q.pop();
+//             v.push_back(x->val);
+                
+//                 if(x->left!=NULL) q.push(x->left);
+//                 if(x->right!=NULL) q.push(x->right);
+                
+//             }
+//             ans.push_back(v);
+            
+//         }
+//         return ans;
