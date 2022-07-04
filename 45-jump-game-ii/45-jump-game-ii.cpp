384e2375ddb2b080a0cbbf7c32446484405f7bdf
Rajat Dua <rdua3561@gmail.com>
2022-07-04 07:12:49 +0530

    Time: 467 ms (29.09%), Space: 18.2 MB (5.98%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int help(int i,vector<int>& nums,int n,vector<int> & dp)
+    {
+        if(i==n-1) return 0;
+        
+        int mini=10001;
+        if(dp[i]!=-1) return dp[i];
+        for(int j=1;j<=nums[i];j++)
+        {
+            if(i+j<n)
+                mini=min(mini,1+help(i+j,nums,n,dp));
+        }
+        return dp[i]=mini;
+    }
+ 
+    
+
+    
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return help(0,nums,n,dp);
+    }
+};
