5a873b7fb59030fbaf31bf9741baca4856d3bdf0
Rajat Dua <rdua3561@gmail.com>
2022-07-10 03:01:44 +0530

    Time: 4 ms (72.78%), Space: 13.3 MB (81.66%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -12,25 +12,22 @@
 class Solution {
 public:
     void help(TreeNode* root, int &sum)
-    {
-        if(root==NULL) return;
-        
-        if(root->left!=NULL and root->left->left==NULL and root->left->right==NULL)
-            sum+=root->left->val ;
-        
-             help(root->left,sum);
-             help(root->right,sum);
-        
-        
-        
-    }
-    int sumOfLeftLeaves(TreeNode* root) {
-        
-        int sum=0;
-        
-        help(root,sum);
-        return sum;
-        
-        
-    }
+{
+    if(root==NULL) return;
+    
+    if(root->left!=NULL and root->left->left==NULL and root->left->right==NULL)
+        sum+=root->left->val ;
+    
+     help(root->left,sum);
+     help(root->right,sum);
+}
+
+int sumOfLeftLeaves(TreeNode* root) 
+{
+    int sum=0;
+    
+    help(root,sum);
+    return sum;
+    
+}
 };
