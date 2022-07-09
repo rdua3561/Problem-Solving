2988c848c7b2c7b6188dee32c568df3a72830723
Rajat Dua <rdua3561@gmail.com>
2022-07-10 03:31:15 +0530

    Time: 92 ms (88.65%), Space: 41.8 MB (89.84%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+//         int n=people.size();
+//         int c=0;
+//         if(n==0) return 0;
+//         if(n==1)
+//         {
+//             if(people[0]<=limit)
+//                 return 1;
+//             else
+//                 return 0;
+//         }
+//         if(n>=2)
+//         {
+//         for(int i=0;i<n;i++)
+//         {
+             
+//             if(i<n-1 and people[i]+people[i+1]<=limit)
+//                 c++;
+            
+//             else if(people[i]<=limit)
+//                 c++;
+            
+          
+//         }
+//         }
+//         return c;
+//     }
+        
+        sort(people.begin(),people.end());
+        
+        int i = 0, j = people.size() - 1,cnt = 0;
+        
+        while(i <= j)
+        {   
+            // lightest person + heaviest person sum <= limit
+            // they can go together
+            if(people[i] + people[j] <= limit)
+            {
+                i++;
+                j--;
+                cnt++;
+            }
+            else if(people[j]<=limit){
+                j--;
+                cnt++;
+            }
+            
+            
+              
+        }
+        
+        return cnt;
+        
+    }
+};
