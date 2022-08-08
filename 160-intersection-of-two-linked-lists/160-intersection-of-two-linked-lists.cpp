179152eb54c58ef6a2352f1d2e61a1de3713aa4e
Rajat Dua <rdua3561@gmail.com>
2022-08-08 08:27:42 +0530

    Time: 56 ms (75.20%), Space: 14.4 MB (88.50%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,24 +9,50 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> s;
+        //Method 1: Using Set
+//         unordered_set<ListNode*> s;
         
-        while(headA!=NULL)
-        {
-            s.insert(headA);
-            headA=headA->next;
-        }
+//         while(headA!=NULL)
+//         {
+//             s.insert(headA);
+//             headA=headA->next;
+//         }
         
-        while(headB!=NULL)
+//         while(headB!=NULL)
+//         {
+//             if(s.find(headB)!=s.end())
+//             {
+//                 return headB;
+//             }
+//             s.insert(headB);
+//             headB=headB->next;
+//         }
+        
+//         return NULL;
+        
+        //Method 2: Using Trick
+        ListNode *water_man=headA;
+        ListNode *fire_man=headB;
+        if(headA==NULL || headB==NULL) return NULL;
+        
+        while(water_man!=fire_man)
         {
-            if(s.find(headB)!=s.end())
+            if(water_man==NULL)
             {
-                return headB;
+                water_man=headB;
             }
-            s.insert(headB);
-            headB=headB->next;
+            else  water_man=water_man->next;
+             if(fire_man==NULL)
+            {
+                fire_man=headA;
+            }
+            else fire_man=fire_man->next;
+            if(water_man==NULL && fire_man==NULL) return NULL;
+            
+           
+            
         }
+        return water_man;
         
-        return NULL;
     }
 };
