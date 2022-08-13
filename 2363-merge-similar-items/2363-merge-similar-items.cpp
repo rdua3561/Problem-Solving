9431f1aea43460ca58c8081abf39ca25c6abf4c9
Rajat Dua <rdua3561@gmail.com>
2022-08-13 14:10:26 +0530

    Time: 38 ms (100.00%), Space: 16.9 MB (40.00%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.cpp
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& item1, vector<vector<int>>& item2) {
-        map<int,int> mp;
+       map<int,int> mp;
+        
         vector<vector<int>> ans;
-        for(auto it: item1)
+        
+        for(int i=0;i<item1.size();i++)
         {
-            mp[it[0]]=mp[it[0]]+it[1];
+            //mp[it[0]]=mp[it[0]]+it[1];
+            mp[item1[i][0]]=item1[i][1];
         }
         
-         for(auto it: item2)
+       for(int i=0;i<item2.size();i++)
         {
-            mp[it[0]]=mp[it[0]]+it[1];
+            //mp[it[0]]=mp[it[0]]+it[1];
+           //if(mp.count(item2[i][0]))
+            mp[item2[i][0]]+=item2[i][1];
         }
         
          for(auto it: mp)
