0938a60d93edd0403d2336528e29206671e5f736
Rajat Dua <rdua3561@gmail.com>
2022-09-11 23:45:38 +0530

    Time: 58 ms (23.64%), Space: 11.9 MB (18.87%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -23,5 +23,28 @@ public:
         }
         return res;
         
+//         int ans=0;
+//         int n=s.size();
+//         //unordered_map<char,int> mp;
+       
+//         for(int i=0;i<n;i++)
+//         {
+//             int res=0;
+//             for(int j=i;j<n;j++)
+//             {
+//                 if(s[i]!=s[j])
+//                 {
+//                     res++;
+                    
+//                 }
+//                 else
+//                 {
+//                     break;
+//                 }
+//             }
+//             ans=max(ans,res);
+//         }
+//         return ans;
+        
     }
 };
