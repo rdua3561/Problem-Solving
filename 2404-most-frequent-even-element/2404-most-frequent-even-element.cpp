15735e4f58121962a0035c0adaa1f2ab14fb495e
Rajat Dua <rdua3561@gmail.com>
2022-09-13 00:10:39 +0530

    Time: 591 ms (25.00%), Space: 46.9 MB (25.00%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        map<int,int> mp;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        int mx=-1;
+        int ans=-1;
+        for(auto it: mp)
+            cout<<it.first<<" "<<it.second;
+        for(auto it: mp)
+        {
+            
+            if((it.first)%2==0 and it.second>mx)
+            {
+                mx=it.second;
+                ans=it.first;
+            }
+        }
+        
+        return ans;
+    }
+};
