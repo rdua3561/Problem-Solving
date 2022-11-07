2b94189c270ad22f5478ea310a5d4039911cee58
Rajat Dua <rdua3561@gmail.com>
2022-11-07 09:32:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.56%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -3,12 +3,20 @@ public:
     int maximum69Number (int num) {
         string s=to_string(num);
         
-        for(auto &it: s)
+//         for(auto &it: s)
+//         {
+//            if(it=='6') {
+//                it='9';
+//                break;
+//            }
+//         }
+        for(int i=0;i<s.length();i++)
         {
-           if(it=='6') {
-               it='9';
-               break;
-           }
+            if(s[i]=='6')
+            {
+                s[i]='9';
+                break;
+            }
         }
         
             
